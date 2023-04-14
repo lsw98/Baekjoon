807fe005993021ff91afa4e90bcd744f142f060d
lsw98 <42402752+lsw98@users.noreply.github.com>
2023-04-14 10:34:27 +0900

    [Gold II] Title: 디스크 트리, Time: 2632 ms, Memory: 55208 KB -BaekjoonHub

---
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static node trie;
+	static class node {
+		Map<String, node> child = new TreeMap<String, node>();
+		boolean end;
+		int depth;
+		
+		public node(int depth) {
+			this.depth = depth;
+		}
+		
+		void insert(String str) {
+			node current = this;
+			String[] dir = str.split("\\\\");
+			for(int i = 0; i < dir.length; i++) {
+				int d = current.depth;
+				current = current.child.computeIfAbsent(dir[i], key -> new node(d+1));
+			}
+		}
+		
+		void print() {
+			for(String s : child.keySet()) {
+				for(int i = 1; i < child.get(s).depth; i++) {
+					System.out.print(" ");
+				}
+				System.out.println(s);
+				child.get(s).print();
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		trie = new node(0);
+		for(int i = 0; i < n; i++) {
+			String line = br.readLine();
+			trie.insert(line);
+		}
+		trie.print();
+	}
+
+}
